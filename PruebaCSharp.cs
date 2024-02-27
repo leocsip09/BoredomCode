fa3dd92a5d4b874cef7d65a626ecadf33887a2de
leocsip09 <lponze@unsa.edu.pe>
2024-02-27 18:58:14 -0500

    Prueba y hola mundo en C#.

---
+++ PruebaCSharp.cs
@@ -0,0 +1,13 @@
+﻿namespace FunStuff;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola mundo");
+        for (int i = 0; i < 8; i++)
+        {
+          Console.WriteLine(i);
+        }
+    }
+}
